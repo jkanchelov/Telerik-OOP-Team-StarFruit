27f1a9f44a4455d2db2fd8b7aad655cbf8d4d00c
Borislav Dimitrov <indepndent1@gmail.com>
2017-01-10 19:03:46 +0200

    Added Ebook class

---
+++ TeamworkStarFruit/CatalogueLib/Products/Mobile/Ebook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogueLib.Products.Enumerations;
+
+namespace CatalogueLib.Products
+{
+    public class EBook : Mobile
+    {
+        public EBook(bool isAvailable, Brand brand) : base(isAvailable, brand)
+        {
+        }
+
+        public string Brand
+        { get; private set; }
+
+        public string Model
+        { get; private set; }
+
+        public double DisplaySize
+        { get; private set; }
+
+        public int Memory
+        { get; private set; }
+
+        public double CPU
+        { get; private set; }
+
+        public int Price
+        { get; private set; }
+    }
+}
