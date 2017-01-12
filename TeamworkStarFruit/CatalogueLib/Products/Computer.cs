f33eb452b44ba83ccde4363306d4e3d470f3b8b5
CvetomirCv <cvetomiriv.cvetkov@gmail.com>
2017-01-12 13:01:01 +0200

    Add files via upload

--- TeamworkStarFruit/CatalogueLib/Products/Computer.cs
+++ TeamworkStarFruit/CatalogueLib/Products/Computer.cs
@@ -9,5 +9,38 @@ namespace CatalogueLib
         {
 
         }
+
+        public string CPU
+        { get; private set; }
+
+        public int DriveMemory //in megabytes
+        { get; private set; }
+
+        public string VideoCardModel
+        { get; private set; }
+
+        public string OperationSystem
+        { get; private set; }
+
+        public double ScreenSize //in inches
+        { get; private set; }
+
+        public int RAM //in gigs
+        { get; private set; }
+
+        public override string ToString()
+        {
+
+            if (this.isAvailable)
+            {
+                return $"Brand: {this.brand}\nCPU:{this.CPU}\nDrive Memory:{this.DriveMemory} megabytes\nVideo Card:{this.VideoCardModel}\nOperation System:{this.OperationSystem}\nScreen size:{this.ScreenSize} inches\nRAM:{this.RAM}";
+            }
+            else
+            {
+                return $"The selected product is not available at the moment.";
+            }
+
+        }
+
     }
 }
--- TeamworkStarFruit/CatalogueLib/Products/SmallAppliances.cs
+++ TeamworkStarFruit/CatalogueLib/Products/SmallAppliances.cs
@@ -5,8 +5,31 @@ namespace CatalogueLib
     public abstract class SmallAppliances : Product
     {
         public SmallAppliances(decimal price, bool isAvailable, Brand brand)
-            :base(price, isAvailable, brand)
+            : base(price, isAvailable, brand)
         {
+
+        }
+
+        public double Capacity
+        { get; private set; }
+
+        public double CableLength
+        { get; private set; }
+
+        public int Affixes
+        { get; private set; }
+
+        public override string ToString()
+        {
+
+            if (this.isAvailable)
+            {
+                return $"Capacity: {this.Capacity}\nCable length:{this.CableLength}\nAffixes: {this.Affixes}";
+            }
+            else
+            {
+                return $"The selected product is not available at the moment.";
+            }
 
         }
     }
