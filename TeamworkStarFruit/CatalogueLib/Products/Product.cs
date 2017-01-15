3ce25d94fc431ca47b17b79f78907ebbd1bc3f28
velev1 <velev_bg@mail.bg>
2017-01-15 02:05:16 +0200

    Updated ToString() method in the class Product.

--- TeamworkStarFruit/CatalogueLib/Products/Product.cs
+++ TeamworkStarFruit/CatalogueLib/Products/Product.cs
@@ -1,13 +1,15 @@
 ﻿namespace CatalogueLib
 {
+    using System.Text;
+
     using CatalogueLib.Products.Enumerations;
 
     public abstract class Product
     {
         public decimal Price { get; private set; }
-        public bool isAvailable { get; private set; }
+        public bool IsAvailable { get; private set; }
         public int Count { get; private set; }
-        public Brand brand { get; private set; }
+        public Brand Brand { get; private set; }
         public int ID { get; private set; }
 
         public Product()
@@ -19,13 +21,20 @@
         {
             this.ID = ID;
             this.Price = price;
-            this.isAvailable = isAvailable;
-            this.brand = brand;
+            this.IsAvailable = isAvailable;
+            this.Brand = brand;
         }
 
         public override string ToString()
         {
-            return $"\nBrand: {this.brand}\nProduct is available: {this.isAvailable}\n Price: {this.Price}";
+            StringBuilder output = new StringBuilder();
+            output.Append('-', 42);
+            output.AppendLine();
+            output.AppendLine(string.Format("ID:[{0}] Brand:{1} available:{2} Price:{3}",
+                this.ID,this.Brand,
+                this.IsAvailable == true?"Yes":"No",
+                this.Price));
+            return output.ToString();
         }
     }
 }
